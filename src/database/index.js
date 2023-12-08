9643826f69d081913f9484c070e5e9ddfece39ae
camilyrackel <112406974+camilyrackel@users.noreply.github.com>
2023-12-08 09:46:12 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,7 @@ async function conectar() {
 
   const pool = new Pool({
     //Padrão da url: postgres://aluno_[matrícula]:[Seis primeiros digitos do CPF]@l177.136.201.182:5439/temp?schema=aluno_[matrícula]
-    connectionString: 'postgres://Aluno_20191214010032:708280@177.136.201.182:5439/temp?schema=aluno_20191214010032'
+    connectionString: 'postgres://Aluno_20201214010008:111315@177.136.201.182:5439/temp?schema=aluno_20201214010008'
   });
 
   //apenas testando a conexão
